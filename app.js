d39b0df65d693f9173ee7ad6822aeace11df9cf5
ItsAbduu <abduux21@gmail.com>
2025-01-16 18:35:58 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const copy = document.getElementById("copy");
+
+let date = new Date();
+
+let year = date.getFullYear();
+
+copy.innerText += " " + year;
+copy.style.fontWeight = "600";
+
+const hum = document.querySelector("#hamburger");
+const nav = document.querySelector(".mobile-nav");
+
+hum.addEventListener("click", function (e) {
+  e.stopPropagation();
+  nav.classList.toggle("visible");
+});
+
+document.body.addEventListener("click", function () {
+  if (nav.classList.contains("visible")) {
+    nav.classList.remove("visible");
+  }
+});
